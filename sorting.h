05ffbab8597cfa7a6ec6dd01252f4943addbd529
Syaula <syaulabalqiea@gmail.com>
2026-06-08 14:03:17 +0700

    Update sorting.h

--- sorting.h
+++ sorting.h
@@ -1,1 +1,16 @@
+#ifndef SORTING_H
+#define SORTING_H
 
+typedef struct
+{
+    long long comparisons;
+    long long swaps;
+} SortStats;
+
+void bubbleSort(int arr[], int n, SortStats *stats);
+void selectionSort(int arr[], int n, SortStats *stats);
+void insertionSort(int arr[], int n, SortStats *stats);
+void mergeSort(int arr[], int n, SortStats *stats);
+void quickSort(int arr[], int n, SortStats *stats);
+
+#endif
