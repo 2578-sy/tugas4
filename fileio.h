dfc7a77b6c8dcd3cec60951236839545db7ea818
Syaula <syaulabalqiea@gmail.com>
2026-06-08 14:40:51 +0700

    Update fileio.h

--- fileio.h
+++ fileio.h
@@ -1,1 +1,13 @@
+#ifndef FILEIO_H
+#define FILEIO_H
 
+#define MAX_WORDS 100000
+#define MAX_LENGTH 100
+
+extern char words[MAX_WORDS][MAX_LENGTH];
+extern char tempWords[MAX_WORDS][MAX_LENGTH];
+extern int wordCount;
+
+int loadFile(const char *filename);
+
+#endif
