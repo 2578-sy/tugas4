10bf7da7698f12b37bec1c8fdbee079caa3f726c
Syaula <syaulabalqiea@gmail.com>
2026-06-08 14:04:46 +0700

    Update sorting.c

--- sorting.c
+++ sorting.c
@@ -1,1 +1,30 @@
+#include <stdlib.h>
+#include "sorting.h"
 
+void bubbleSort(int arr[], int n, SortStats *stats)
+{
+    int i, j;
+
+    for(i = 0; i < n - 1; i++)
+    {
+        int swapped = 0;
+
+        for(j = 0; j < n - i - 1; j++)
+        {
+            stats->comparisons++;
+
+            if(arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+
+                stats->swaps++;
+                swapped = 1;
+            }
+        }
+
+        if(!swapped)
+            break;
+    }
+}
