89a83d924907c168409344fc658d65244027c988
Syaula <syaulabalqiea@gmail.com>
2026-06-08 14:42:50 +0700

    Refactor sorting functions to use SortStats pointer

--- sorting.h
+++ sorting.h
@@ -5,12 +5,13 @@ typedef struct
 {
     long long comparisons;
     long long swaps;
+    double timeUsed;
 } SortStats;
 
-void bubbleSort(int arr[], int n, SortStats *stats);
-void selectionSort(int arr[], int n, SortStats *stats);
-void insertionSort(int arr[], int n, SortStats *stats);
-void mergeSort(int arr[], int n, SortStats *stats);
-void quickSort(int arr[], int n, SortStats *stats);
+void insertionSort(SortStats *s);
+void bubbleSort(SortStats *s);
+void selectionSort(SortStats *s);
+void mergeSort(SortStats *s);
+void quickSort(SortStats *s);
 
 #endif
